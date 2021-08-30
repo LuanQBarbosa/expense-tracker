580c10167aa9fa18de10a506e0e6ff55e798a272
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-30 15:05:03 -0300

    Project structure changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses({ items }) {
+const Expenses = ({ items }) => {
     const expenseItems = items.map(expense => {
         return (
             <ExpenseItem
