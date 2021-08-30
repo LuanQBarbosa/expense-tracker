022376122cf7e9dd72354698bf29a1625b672ef1
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-30 09:43:42 -0300

    ExpenseDate component implementation and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({ title, amount, date }) {
+    
+    
     return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <ExpenseDate date={date} />
+
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
