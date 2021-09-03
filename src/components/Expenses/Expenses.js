8dddfadcb8115ac7f7d80e345b71a80384b813be
Luan Barbosa <luanqbarbosa@gmail.com>
2021-09-03 16:49:26 -0300

    ExpensesFilter component created to filter expenses by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = ({ items }) => {
+    const [filteredYear, setFilteredYear] = useState('2021');
+
     const expenseItems = items.map(expense => {
         return (
             <ExpenseItem
@@ -16,10 +19,17 @@ const Expenses = ({ items }) => {
         );
     });
 
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
-        <Card className="expenses">
-            {expenseItems}
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {expenseItems}
+            </Card>
+        </div>
     );
 }
 
